fa9c501f0918a368efc741cab6eca08ea7983676
Dhruv Tewari <dhrutewa@gmail.com>
2024-09-14 21:36:12 -0500

    setup file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="resturant_forecast",
+    version="0.0.0",
+    author="Dhruv Tewari",
+    author_email="dhrutewa@gmail.com",
+    packages=find_packages()
+)
